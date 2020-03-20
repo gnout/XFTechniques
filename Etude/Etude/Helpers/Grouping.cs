a4d3eeaa9e0295c1ec385a08007ec0334739b371
Dimitris Mylonas <gnout@hotmail.com>
2020-03-21 00:01:28 +0100

    Add Expanded/Collapse List

--- Etude/Etude/Helpers/Grouping.cs
+++ Etude/Etude/Helpers/Grouping.cs
@@ -1,15 +1,34 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace Etude.Helpers
 {
     public class Grouping<K, T> : ObservableCollection<T>
     {
         public K Key { get; private set; }
+        public string StateIcon => Expanded ? "collapsed" : "expanded";
 
-        public Grouping(K key, IEnumerable<T> items)
+        public bool Expanded
+        {
+            get => _expanded;
+            set
+            {
+                if (_expanded != value)
+                {
+                    _expanded = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("Expanded"));
+                    OnPropertyChanged(new PropertyChangedEventArgs("StateIcon"));
+                }
+            }
+        }
+
+        private bool _expanded;
+
+        public Grouping(K key, IEnumerable<T> items, bool expanded = false)
         {
             Key = key;
+            Expanded = expanded;
 
             foreach (var item in items)
             {
--- Etude/Etude/ViewModels/Set01Example04ViewModel.cs
+++ Etude/Etude/ViewModels/Set01Example04ViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using Etude.Helpers;
 using Etude.Models;
 using Etude.Services;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Etude.ViewModels
@@ -19,12 +21,20 @@ namespace Etude.ViewModels
         private ObservableCollection<Grouping<string, Beer>> _model;
         #endregion
 
+        #region Delegate Commands
+        public ICommand ExpandCollapseCommand { get; }
+        #endregion
+
         #region Services
         private readonly DataService _dataService = new DataService();
         #endregion
 
+        private ObservableCollection<Grouping<string, Beer>> _staticData;
+
         public Set01Example04ViewModel()
         {
+            ExpandCollapseCommand = new Command<object>(ExecuteExpandCollapseCommand);
+
             LoadDataCommand = new Command(() => Init());
         }
 
@@ -34,9 +44,40 @@ namespace Etude.ViewModels
 
             try
             {
-                Model = _dataService.GroupBeerList(_dataService.GetBeers().ToList<Beer>());
+                _staticData = _dataService.GroupBeerList(_dataService.GetBeers().ToList<Beer>());
+                UpdateListContent();
             }
             finally { IsBusy = false; }
+        }
+
+        private void ExecuteExpandCollapseCommand(object item)
+        {
+            var tappedGroup = (Grouping<string, Beer>)item;
+            var selectedIndex = _model.IndexOf(tappedGroup);
+
+            if (selectedIndex >= _staticData.Count)
+                return;
+
+            _staticData[selectedIndex].Expanded = !_staticData[selectedIndex].Expanded;
+
+            UpdateListContent();
+        }
+
+        private void UpdateListContent()
+        {
+            Model = new ObservableCollection<Grouping<string, Beer>>();
+
+            foreach (var beerGroup in _staticData)
+            {
+                var list = new List<Beer>();
+
+                if (beerGroup.Expanded)
+                {
+                    list = new List<Beer>(beerGroup);
+                }
+
+                Model.Add(new Grouping<string, Beer>(beerGroup.Key, list, beerGroup.Expanded));
+            }
         }
     }
 }
